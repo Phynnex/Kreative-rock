630fda4a6e2f187fa4e8b9077ac8cb033a540111
Festus Alabo Augustine <43374293+Asoft123@users.noreply.github.com>
2022-01-24 19:51:32 +0100

    fixed event bubbling issue on dashboad

--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -52,7 +52,7 @@ export const DashboardBody = styled.div`
         width: 100%;
         height:100%;
         overflow-y: scroll;
-        margin-top:115px;
+        margin-top:125px;
     `} 
 	${media.smallMobile`
         width: 100%;
