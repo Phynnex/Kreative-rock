08ec38454a416a81c1458e6a37816da7ee98d506
Phynnex <angalafynetonye@gmail.com>
2022-02-07 18:38:48 +0100

    added social links

--- src/pages/dashboard/home/index.jsx
+++ src/pages/dashboard/home/index.jsx
@@ -1,12 +1,11 @@
 // import ActionBtnDashboard from "components/dashboard/actionbuttons"
-import DashboadHero from "components/dashboard/DashboardHero";
 import React from "react";
 
 function DashboardPage() {
   return (
     <div>
       {/* <ActionBtnDashboard /> */}
-      <DashboadHero />
+      
       <h2>Dashboard Page</h2>
     </div>
   );
